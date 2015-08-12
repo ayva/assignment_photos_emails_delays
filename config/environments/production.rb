1e0b411f1e30df128312e862661774594a779f6f
ayva <olga.ayva@gmail.com>
2015-08-12 10:12:27 -0700

    added host to make dyne process jobs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,8 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
   config.action_mailer.delivery_method ||= :smtp
-  
+  config.action_mailer.default_url_options = { :host => "example.com" }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
